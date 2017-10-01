a0e08aa8bf72ed61546cfb37427514fb1b602cb0
The-DarkBeast <pranaynarang@gmail.com>
2017-10-01 14:36:52 +0000

    soong: Initial re-factor to FH
    Change-Id: Ic3fe16ea505d604f6b29cdc366f3472de7080f66
    Signed-off-by: The-DarkBeast <pranaynarang@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -20,7 +20,7 @@ import (
 	"runtime"
 	"strings"
 
-	"lineage/soong/android"
+	"fh/soong/android"
 
 	"github.com/google/blueprint/proptools"
 )
@@ -88,8 +88,8 @@ type variableProperties struct {
 			Cppflags []string
 		}
 
-		// include Lineage variables
-		Lineage android.Product_variables
+		// include FireHound variables
+		Fire android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -156,8 +156,8 @@ type productVariables struct {
 
 	Override_rs_driver *string `json:",omitempty"`
 
-	// include Lineage variables
-	Lineage android.ProductVariables
+	// include FireHound variables
+        Fire android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
